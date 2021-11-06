bf27519ba2445d8def9ae22fa09d974f9cd36c9d
V Sanjay <sanjayvihaan111@gmail.com>
2021-11-06 21:00:51 +0530

    first commit

---
+++ day-1/var_data_type.py
@@ -0,0 +1,79 @@
+#Variable --- Help to store the value and which we can access.
+# print("Hello world!") #With out a variable
+
+# smple = 1
+# # sample = "Hello world!!" # sample is a variable and hello world is a value
+# print(smple)
+
+#Number
+#strings
+# List
+# float
+
+#-----------------------------------------------------------------------
+
+#Assignment
+
+#simple message: Store a message in a vairable, and print that message. 
+# Then change the value of your variable to a new message and print the new message
+
+# message = "Hello it is time up to gain the knowledge"
+# print(message)
+# New_message = "You have learnt Variable in Python"
+# print(New_message)
+
+
+#--------------------------------------------------------------------------------
+
+#STRINGS
+# Strings is a simply a series of character. Anything inside the code is a string in python.
+
+"This is a string"
+'This a also a string'
+#This is comment
+
+#Change Case--- Upper case and lower case, title
+name = "tharun is learing Python"
+print(name.title())
+print(name.upper())
+
+name = "THE PYTHON IS GOOD"
+print(name.lower())
+
+#---------------------------------------------------
+
+#Combining the string:
+
+first_Name = "tharun"
+last_Name = "S"
+full_Name = first_Name + " " + last_Name
+print("Hello " + full_Name.upper() +" welcome to python learning")
+
+#-------------------------------------------------------
+
+#Adding whitespace to string with Tabs or Newlines:
+name = "THE PYTHON IS GOOD"
+print("\tTHE PYTHON IS GOOD")
+#Lang:
+    #python
+    #Java
+    #JavaScript
+print("Lang:\n\tPython\n\tJava\n\tJavaScript")
+#--------------------------------------
+
+#stripping Whitespace
+
+learn_lang = "  Python   "
+print(learn_lang.rstrip())
+print(learn_lang.lstrip())
+print(learn_lang.strip())
+
+# Avoiding Error
+message = 'One of python "strength" is its dicerse community'
+print(message)
+
+#Number:
+# *Integers
+#* Floats
+
+#List:
